65d49153f28b2903ca89e24f82788fd7183f721f
khainghsuthwe <khaingjar@gmail.com>
2025-11-02 21:20:38 +0630

    [fix] fix issues of mkdir issue on vercel

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -9,7 +9,10 @@ const fs = require("fs");
 
 // Ensure uploads directory exists
 const UPLOADS_DIR = path.join(__dirname, "../../uploads");
-if (!fs.existsSync(UPLOADS_DIR)) {
+if (process.env.VERCEL) {
+  console.log("Running on Vercel — skipping local upload directory setup");
+}
+else(!fs.existsSync(UPLOADS_DIR)) {
   fs.mkdirSync(UPLOADS_DIR, { recursive: true });
 }
 
